375cfafa17cdff6b078ef6e45e52d0c8a604af57
Santos Alarcón <santosalarcon86@gmail.com>
2023-06-02 19:22:22 +0200

    Renamed publicidad class to publi

--- src/components/Publi.jsx
+++ src/components/Publi.jsx
@@ -4,7 +4,7 @@ import "../styles/Publi.css";
 
 const Publi = () => {
 	return (
-		<section id="publicidad">
+		<section id="publi">
 			<a href="https://www.revolut.com/es-ES/promo/velada-de-ibai/" target="_blank" rel="noreferrer"><img src={revolut} /></a>
 			<a href="https://cloud.e.alsa.com/veladaIII?utm_source=c%C3%B3mo_llegar&utm_medium=web&utm_campaign=2023_07_lavelada&utm_content=abr-jul" target="_blank" rel="noreferrer"><img src={alsa} /></a>
 		</section>
